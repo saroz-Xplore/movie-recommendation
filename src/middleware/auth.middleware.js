2bd55f3245ebbe8f801f7bff482f5c16887eb7f1
Saroj <sarojsimkhada2@gmail.com>
2025-03-22 22:19:30 +0545

    jwt

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,35 @@
+import { User } from "../models/user.models.js";
+import jwt from "jsonwebtoken"
+
+
+export const VerifyToken = async(req, res, next) => {
+    try {
+        
+        const token= req.cookies?.accessToken
+
+
+        if(!token){
+            res.status(401).json({
+                message: "Unauthorized request"
+            })
+        }
+
+        const decodedToken= jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+        
+        if(!user){
+            res.status(404).json({
+                message: "Error User"
+            })
+        }
+        req.user = user
+        
+        next()
+    } catch (error) {
+        res.status(401).json({
+            message:error.message
+        })
+    }
+}
